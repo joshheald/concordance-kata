5ec9a49a1baedcff4b900ad0d824dead9fdb4dc5
Josh Heald <josh.heald@kotikan.com>
2015-10-06 21:19:25 +0100

    Implemented concordanceForString to build a Concordance correctly

--- concordancegenerator.py
+++ concordancegenerator.py
@@ -1,3 +1,5 @@
+import concordance
+
 class ConcordanceGenerator():
 	"""Produces a concordance, a list of all the words present in a text, and the line numbers on which they occur."""
 	splitter = None
@@ -5,9 +7,15 @@ class ConcordanceGenerator():
 		self.splitter = splitter
 
 	def concordanceForString(self, string=None):
-		lines = self.splitter.linesForString(string)
-		for line in lines:
-			self.splitter.lineSplit(line)
-		return string
+		if string is not None:
+			lines = self.splitter.linesForString(string)
+			c = concordance.Concordance()
+			for line_number, line in enumerate(lines, start=1):
+				words = self.splitter.lineSplit(line)
+				if words is not None:
+					for word in words:
+						c.addInstanceOfWordAtLine(word, line_number)
+
+			return c
 
 		
--- concordancegeneratortests.py
+++ concordancegeneratortests.py
@@ -1,52 +1,86 @@
 import unittest
 import concordancegenerator
+import concordance
 
 class ConcordanceGeneratorTests(unittest.TestCase):
 	def test_init_takesASplitter(self):
 		"""A splitter is passed in to do the work of splitting up input strings"""
-		sut = concordancegenerator.ConcordanceGenerator(splitter=MockSplitter())
+		sut = concordancegenerator.ConcordanceGenerator(splitter=SpySplitter())
 
 	def test_concordanceForString_whenCalledWithNoneReturnsNone(self):
 		"""An empty string wouldn't make sense if there was no text to begin with"""
-		sut = concordancegenerator.ConcordanceGenerator(splitter=MockSplitter())
+		sut = concordancegenerator.ConcordanceGenerator(splitter=SpySplitter())
 		self.assertIsNone(sut.concordanceForString(None))
 
-	def test_concordanceForString_whenCalledWithAnEmptyStringReturnsAnEmptyString(self):
+	def test_concordanceForString_whenCalledWithAnEmptyStringReturnsAnEmptyConcordance(self):
 		"""An empty string has no words, so the concordance should be empty too"""
-		sut = concordancegenerator.ConcordanceGenerator(splitter=MockSplitter())
-		self.assertEqual("", sut.concordanceForString(""))
+		sut = concordancegenerator.ConcordanceGenerator(splitter=SpySplitter())
+		self.assertIsNone(sut.concordanceForString())
 
 	def test_concordanceForString_usesTheSplitterItWasCreatedWithToSplitTheStringIntoLines(self):
 		"""We have to parse the string one line at a time in order to retain the line numbers"""
-		mockSplitter = MockSplitter()
-		sut = concordancegenerator.ConcordanceGenerator(splitter=mockSplitter)
+		spySplitter = SpySplitter()
+		sut = concordancegenerator.ConcordanceGenerator(splitter=spySplitter)
 		expected_string = "Hello world!\nGoodbye cruel world..."
 		sut.concordanceForString(expected_string)
-		self.assertTrue(mockSplitter.linesForString_was_called)
-		self.assertEqual(expected_string, mockSplitter.linesForString_most_recent_string)
+		self.assertTrue(spySplitter.linesForString_was_called)
+		self.assertEqual(expected_string, spySplitter.linesForString_most_recent_string)
 
 	def test_concordanceForString_usesTheSplitterItWasCreatedWithToSplintEachLineIntoWords(self):
-		mockSplitter = MockSplitter()
-		sut = concordancegenerator.ConcordanceGenerator(splitter=mockSplitter)
+		mock_splitter = MockSplitter()
+		sut = concordancegenerator.ConcordanceGenerator(splitter=mock_splitter)
 		expected_string = "Hello world!\nGoodbye cruel world..."
 		sut.concordanceForString(expected_string)
-		self.assertEqual(2, mockSplitter.lineSplit_call_count)
-		self.assertEqual(["Hello world!", "Goodbye cruel world..."], mockSplitter.lineSplit_lines)
+		self.assertEqual(["Hello world!", "Goodbye cruel world..."], mock_splitter.lineSplit_lines)
+
+	def test_concordanceForString_returnsAConcordanceWithAllTheWordsReturnedFromLineSplit(self):
+		expected_concordance = concordance.Concordance()
+		expected_concordance._wordlist = {
+			"hello": set([1]),
+			"world": set([1, 2]),
+			"goodbye": set([2]),
+			"cruel": set([2])
+		}
+		mock_splitter = MockSplitter()
+		sut = concordancegenerator.ConcordanceGenerator(splitter=mock_splitter)
+		actual_concordance = sut.concordanceForString("Hello world!\nGoodbye cruel world...")
+		self.assertEqual(expected_concordance.allEntries(), actual_concordance.allEntries())
+
+
 
 
 # Mock Classes
 
-class MockSplitter():
-	lineSplit_call_count = 0
-	lineSplit_lines = []
-	linesForString_was_called = False
-	linesForString_most_recent_string = None
+class SpySplitter():
+	def __init__(self):
+		self.lineSplit_call_count = 0
+		self.linesForString_was_called = False
+		self.linesForString_most_recent_string = None
 
 	def lineSplit(self, line):
 		self.lineSplit_call_count += 1
-		self.lineSplit_lines.append(line)
 
 	def linesForString(self, string):
 		self.linesForString_was_called = True
 		self.linesForString_most_recent_string = string
 		return ["Hello world!", "Goodbye cruel world..."]
+
+class MockSplitter():
+	def __init__(self):
+		self.lineSplit_lines = []
+		self.lineCount = 0
+
+	def lineSplit(self, line):
+		self.lineSplit_lines.append(line)
+		if self.lineCount == 0:
+			self.lineCount += 1
+			return ["Hello", "world"]
+		elif self.lineCount == 1:
+			self.lineCount += 1
+			return ["Goodbye", "cruel", "world"]
+		else:
+			return None
+
+	def linesForString(self, string):
+		return ["Hello world!", "Goodbye cruel world..."]
+		
